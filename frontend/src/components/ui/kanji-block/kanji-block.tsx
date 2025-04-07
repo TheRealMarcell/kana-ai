43e09045533f66e6f5d9c76a109be5cc21263b84
Marcell <marcellusgerson@gmail.com>
2025-04-07 13:24:22 +1000

    commit

---
+++ frontend/src/components/ui/kanji-block/kanji-block.tsx
@@ -0,0 +1,12 @@
+import './Block.css';
+
+
+export const KanjiCard = () => {
+  return(
+    <>
+      <div className="bg-[#ff00aa] w-12 h-12 rounded justify-items-center items-stretch block-shadow">
+        <p className="text-4xl">上</p>
+      </div>
+    </>
+  )
+}
